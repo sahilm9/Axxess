b6319553fb6c6451b66f7dc5a941e232ce0edb05
Sahil Mokkapati <sahil.mokkapati@gmail.com>
2017-12-01 09:55:04 -0600

    correcting to correct id's

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // Selecting elements using dom selectors
 
 var numInput = document.querySelector("input[type='number']");
-var start = document.querySelector("#start");
-var restart = document.querySelector("#restart");
-var resetButton = document.querySelector("#reset");
+var start = document.querySelector("#start-button");
+var restart = document.querySelector("#restart-button");
+var resetButton = document.querySelector("#reset-button");
 var box1 = document.getElementById("box1");
 var box2 = document.getElementById("box2");
 
