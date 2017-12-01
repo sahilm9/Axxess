5093750dd600c2d79256e3e44850cc59f864ad20
Sahil Mokkapati <sahil.mokkapati@gmail.com>
2017-12-01 09:20:10 -0600

    declaring and initializing variables

--- app.js
+++ app.js
@@ -6,3 +6,9 @@ var restart = document.querySelector("#restart");
 var resetButton = document.querySelector("#reset");
 var box1 = document.getElementById("box1");
 var box2 = document.getElementById("box2");
+
+// counter to increment, target for input and gameOver for gamecondition
+
+var counter =0;
+var target;
+var gameOver = false;
