d439097fed7301569a2904dae994a993f4f7de36
Sahil Mokkapati <sahil.mokkapati@gmail.com>
2017-12-01 09:18:25 -0600

    select elements

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+// Selecting elements using dom selectors
 
+var numInput = document.querySelector("input[type='number']");
+var start = document.querySelector("#start");
+var restart = document.querySelector("#restart");
+var resetButton = document.querySelector("#reset");
+var box1 = document.getElementById("box1");
+var box2 = document.getElementById("box2");
